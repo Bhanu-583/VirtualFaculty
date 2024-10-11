5d9a5f5a5e5474b0e84ae6fdfb354972c5ba8963
Bhanu Nedunuri <bhanutejanedunuri.com>
2024-10-11 23:36:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import './App.css'; // Import your CSS file
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
